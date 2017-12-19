2319cc9c87013bf2a957b362a69c8f5d3b2cb839
Niki Skaarup <nikiskaarup@hotmail.com>
2017-12-19 12:21:33 +0100

    asdasd

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const app = express();
+app.use(bodyparser.json());
 const PORT = 3000;
 
 app.get('/:data', (req, res) => {
