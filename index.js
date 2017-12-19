6bda39beacd2bc1a5a5b79c25d94d12404f032ed
Niki Skaarup <nikiskaarup@hotmail.com>
2017-12-19 13:11:00 +0100

    gf

--- index.js
+++ index.js
@@ -10,9 +10,20 @@ app.get('/:data', (req, res) => {
   res.send('OK - GET');
 });
 
+app.get('/', (req, res) => {
+  console.log('GET:');
+  res.send('OK - GET');
+});
+
 app.post('/:data', (req, res) => {
   console.log('POST');
   console.log(`PARAMS: ${req.params['data']}`);
+  console.log(`BODY  : ${JSON.stringify(req.body)}`);
+  res.send('OK - POST');
+});
+
+app.post('/', (req, res) => {
+  console.log('POST');
   console.log(`BODY  : ${JSON.stringify(req.body)}`);
   res.send('OK - POST');
 });
